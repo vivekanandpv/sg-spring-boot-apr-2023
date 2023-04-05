1eb439ce7d77e075db7a28927e0c4b7c79183fc9
Vivekanand P V <vivekanandpv@outlook.com>
2023-04-05 11:45:49 +0530

    api uses the service

--- src/main/java/com/vivekanandpv/springbootstarterproject/apis/PersonsApi.java
+++ src/main/java/com/vivekanandpv/springbootstarterproject/apis/PersonsApi.java
@@ -1,6 +1,7 @@
 package com.vivekanandpv.springbootstarterproject.apis;
 
 import com.vivekanandpv.springbootstarterproject.models.Person;
+import com.vivekanandpv.springbootstarterproject.services.PersonService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,29 +10,36 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/persons")
 public class PersonsApi {
+    private final PersonService service;
+
+    public PersonsApi(PersonService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public ResponseEntity<List<Person>> get() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Person> get(@PathVariable int id) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(service.getById(id));
     }
 
 
     @PostMapping
     public ResponseEntity<Person> create(@RequestBody Person person) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(service.create(person));
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Person> update(@PathVariable int id, @RequestBody Person person) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(service.update(id, person));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Person> delete(@PathVariable int id) {
-        return ResponseEntity.ok().build();
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
