1b02057f18596dc97048bd413edb55154daf5854
Vivekanand P V <vivekanandpv@outlook.com>
2023-04-05 12:09:03 +0530

    service implementation

--- src/main/java/com/vivekanandpv/springbootstarterproject/models/Person.java
+++ src/main/java/com/vivekanandpv/springbootstarterproject/models/Person.java
@@ -5,6 +5,12 @@ public class Person {
     private String lastName;
     private String email;
 
+    public Person(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public String getFirstName() {
         return firstName;
     }
---
+++ src/main/java/com/vivekanandpv/springbootstarterproject/services/PersonServiceImplementation.java
@@ -0,0 +1,39 @@
+package com.vivekanandpv.springbootstarterproject.services;
+
+import com.vivekanandpv.springbootstarterproject.models.Person;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonServiceImplementation implements PersonService {
+    @Override
+    public List<Person> getAll() {
+        return List.of(
+                new Person("John", "Doe", "john@gmail.com"),
+                new Person("Mark", "Doe", "mark@gmail.com"),
+                new Person("Clyde", "Doe", "clyde@gmail.com")
+        );
+    }
+
+    @Override
+    public Person getById(int id) {
+        return new Person("John", "Doe", "john@gmail.com");
+    }
+
+    @Override
+    public Person create(Person person) {
+        return new Person("John", "Doe", "john@gmail.com");
+    }
+
+    @Override
+    public Person update(int id, Person person) {
+        return new Person("John " + id, "Doe", "john@gmail.com");
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
